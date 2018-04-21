9bb4b476c4432a1a957813c1a6d35d4cf9512659
Liz Goff <livingearthcreations@gmail.com>
2018-04-21 12:31:51 -0500

    files connected input fields buttons and table on dom

---
+++ server/public/scripts/client.js
@@ -0,0 +1,16 @@
+console.log('JS')
+
+$(document).ready(onReady);
+console.log('doc ready');
+
+function onReady() {
+    console.log('jquery ready');
+
+    //need  $('#add'.on('click', )
+    //need  $('#enter'.on('click', )
+    //need  $('#subtract'.on('click', )
+}
+
+
+
+// I will need a forEach fir mathHistory table to be appended
---
+++ server/server.js
@@ -0,0 +1,28 @@
+const express =require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 5000;
+
+// place to connect with files
+
+app.use(express.static('server/public'));
+app.use(bodyParser.urlencoded({extended: true}));
+
+// get requests like the one below;
+// app.get('/records', (req, res) => {
+//     res.send(recordCollection);
+// });
+
+// post requests like this one seen below;
+// don't forget the 200!
+// app.post('/add-record', (req, res) => {
+//     console.log(req.body);
+//     recordCollection.push(req.body);
+//     res.sendStatus(200);
+// });
+
+// math shoud be calculated on server side
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+});
